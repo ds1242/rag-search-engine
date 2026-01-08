f22905b18ef3db68f28fc7bb27f24e8c3858ff9c
David Shaw <dshaw11@utk.edu>
2026-01-08 09:02:49 -0500

    setup to embed text to use the models tokenizer

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -4,6 +4,13 @@ class SemanticSearch:
     def __init__(self) -> None:
         self.model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
+    def generate_embedding(self, text):
+        if text == "" or text = ' ':
+            raise ValueError("text string is empty")
+
+        embedding = self.model.encode(list(text))
+        return embedding[0]
+
 
 
 
@@ -11,3 +18,10 @@ def verify_model():
     model = SemanticSearch()
     print(f"Model Loaded: {model.model}")
     print(f"Max sequence length: {model.model.max_seq_length}")
+
+def embed_text(text):
+    model = SemanticSearch()
+    embedding = model.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"Frist 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
