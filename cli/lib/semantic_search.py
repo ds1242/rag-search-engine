df371a2716075bdaa9d76740f9b73529045b42d7
David Shaw <dshaw11@utk.edu>
2026-01-13 11:00:36 -0500

    added chunksemanticsearch class that inherits from semantic search

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -69,6 +69,12 @@ class SemanticSearch:
 
         return final_results
 
+class ChunkedSemanticSearch(SemanticSearch):
+    def __init__(self, model_name = "all-MiniLM-L6-v2") -> None:
+        super().__init__(model_name)
+        self.chunk_embeddings = None
+        self.chunk_metadata = None
+
 
 def verify_embeddings() -> None:
     search_instance = SemanticSearch()
