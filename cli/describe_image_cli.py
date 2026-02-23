8632fc65c82ad1a8311d9ae73f963a36449722d5
David Shaw <dshaw11@utk.edu>
2026-02-23 14:08:56 -0500

    setup search example that can rewrite the query to match the image

--- cli/describe_image_cli.py
+++ cli/describe_image_cli.py
@@ -3,18 +3,13 @@ from lib.describe_image import describe_command
 
 def main():
     parser = argparse.ArgumentParser(description="Describe images CLI")
-    subparser = parser.add_subparsers(dest="command", help="Available commands")
 
-    image_parser = subparser.add_parser(
-        "--image", type=str, help="Path to input image"
-    )
-    image_parse = subparser.add_parser("--query", type=str, help="a text query to rewrite based on the image")
+    parser.add_argument("--image", type=str, required=True, help="Path to input image")
+    parser.add_argument("--query", type=str, required=True, help="a text query to rewrite based on the image")
 
     args = parser.parse_args()
     
-    match args.command:
-        case "image":
-            describe_command(args.image, args.query)
+    describe_command(args.image, args.query)
 
 
 if __name__ == "__main__":
